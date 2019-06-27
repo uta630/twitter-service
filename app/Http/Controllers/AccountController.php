5b4e66332ea89789e9ba221c7b6a5c70539f8cdd
uta630 <usuitkc.630@gmail.com>
2019-06-28 00:07:17 +0900

    DB/Seeder の設定

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -8,7 +8,7 @@ class AccountController extends Controller
 {
     public function __construct()
     {
-        // $this->middleware('auth');
+        $this->middleware('auth');
     }
     
     /*
